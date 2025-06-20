70dccb4a172f1063ff3545b0a117ab5517a391fd
unknown <40200353@esmad.ipp.pt>
2025-06-20 13:59:15 +0100

    Update: simplified scripts

--- src/main.py
+++ src/main.py
@@ -1,140 +1,148 @@
 import os
-import shutil
-import argparse
-import logging
+import copy
 import utils
+import shutil
+import logging
+import argparse
+
+from tabulate import tabulate
 
 # TODO
-# 1 - Change the way ISOs are being extracted so it stops needing 7z installed
-# 2 - Actually write the ordering function
-# 3 - Maybe use a class that gets everything right from the start and then just use the instance's variables
-# 4 - Implement reload option so the games/tools list can be refreshed
-# 5 - Maybe show game list as a table?
-# 6 - Move FOLDERS const to utils
+# 1 - Actually write the ordering function
+# 2 - Implement reload option so the games/tools list can be refreshed
+# 3 - Strikethrough feature
+# 4 - Think about what to log. Right now, the only process being fully logged is the renaming
+# 5 - After renaming, I need to reload games
+# 6 - After ordering, I need to reload games
+# 7 - Submenu for ordering: A-Z, Z-A, divided and not divided by category
+# 8 - Add requirements.txt and complete README 
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
     handlers=[
-        logging.FileHandler("app.log"),
+        logging.FileHandler(utils.join_with_script_path("app.log"), encoding="utf-8"),
         logging.StreamHandler()
     ]
 )
 
-FOLDERS = {
-  "ISO": "ISO",
-  "GAME": "PSP\\GAME"
-}
-
-def list_files(input_dir):
+def list_files(items):
     """
     Lists games/tools, sorted by creation/last modified date.
     """
-    iso_folder = os.path.join(input_dir, FOLDERS["ISO"])
-    psp_game_folder = os.path.join(input_dir, FOLDERS["GAME"])
-
-    try:
-        all_items = utils.filter_directory(utils.parse_directory(iso_folder), utils.SearchTypes.ISO)
-        all_items.extend(utils.filter_directory(utils.parse_directory(psp_game_folder), utils.SearchTypes.DIR))
-        all_items.sort(key=lambda item: item["timestamp"], reverse=True)
-    except:
-        return logging.error(f"An error occurred while parsing the directories")
-
-    if len(all_items) == 0:
+    if len(items) == 0:
         return logging.warning("No games/tools were found")
+        
+    print(tabulate(
+      [[row_idx + 1, items[row_idx]["name"], items[row_idx]["date"]] for row_idx in range(len(items))],
+      headers=['#', 'File name', 'Date']
+    ))
 
-    for index, item in enumerate(all_items):
-        print(f"{index + 1}. {item["name"]} - {item["date"]}")
-
-def rename_files(input_dir):
+def rename_files(items):
     """
     Renames ISO files in the given directory based on their PARAM.SFO titles.
     """
-    temp_dir = os.path.join(os.path.abspath(os.getcwd()), "temp")
-    os.makedirs(temp_dir, exist_ok=True)
-    
-    iso_folder = os.path.join(input_dir, FOLDERS["ISO"])
-    isos = utils.filter_directory(utils.parse_directory(iso_folder), utils.SearchTypes.ISO)
-    
+    isos = [item for item in items if item["category"] == utils.SearchQueries.ISO]
     for iso in isos:
-        iso_path = os.path.join(iso[-1], iso[0])
-        file_name = iso[0][:-4]
-        logging.info(f"Processing: {iso_path}")
+        iso_path, file_name, dir = iso["path"], iso["name"], iso["directory"]
+        
+        if file_name != isos[0]["name"]:
+            logging.info("==========================================")
+        
+        logging.info(f"Processing renaming for '{iso_path}'")
 
+        # Extract PARAM.SFO from the ISO
         try:
-            sfo_path = utils.extract_sfo(iso_path, temp_dir)
-            if not sfo_path or not os.path.isfile(sfo_path):
-                logging.warning(f"Failed to extract PARAM.SFO from {iso_path}. Skipping.")
-                continue
-              
-            title = utils.parse_sfo(sfo_path, "TITLE")
-            if not title:
-                logging.warning(f"Failed to get TITLE from {sfo_path}. Skipping.")
-                continue
-              
-            sanitized_title = utils.sanitize_filename(title)
-            if not sanitized_title:
-                logging.warning(f"Invalid TITLE extracted from {sfo_path}. Skipping.")
-                continue
-
-            new_iso_path = os.path.join(iso[-1], sanitized_title + utils.SearchTypes.ISO.value)
-            os.rename(iso_path, new_iso_path)
-            logging.info(f"Renamed '{file_name}' to '{sanitized_title + utils.SearchTypes.ISO.value}'")
+            sfo = utils.extract_sfo(iso_path)
+            sfo_path, temp_dir = sfo[0], sfo[1]
+            if not os.path.isfile(sfo_path): raise
+            logging.info(f"Successfully extracted PARAM.SFO from ISO")
         except Exception as e:
-            logging.error(f"An error occurred while processing '{file_name}': {e}")
+            utils.log_warning_with_error(f"Error extracting PARAM.SFO from '{iso_path}', skipping", e)
+            continue
+          
+        # Get TITLE param from PARAM.SFO
+        try:
+            title = utils.parse_sfo(sfo_path, "TITLE")
+            logging.info(f"Successfully parsed PARAM.SFO: TITLE = '{title}'")
+        except Exception as e:
+            utils.log_warning_with_error(f"Error getting TITLE from '{sfo_path}', skipping", e)
+            continue
+        
+        # Sanitize TITLE before renaming
+        try:    
+            sanitized_title = utils.sanitize_filename(title)
+            logging.info(f"Successfully sanitized TITLE to: '{sanitized_title}'")
+        except Exception as e:
+            utils.log_warning_with_error(f"Error sanitizing TITLE = '{title}' from '{sfo_path}', skipping", e)
+            continue
+        
+        # Rename the ISO
+        try:
+            new_file_name = sanitized_title + utils.SearchQueries.ISO.value
+            new_iso_path = os.path.join(dir, new_file_name)
+            os.rename(iso_path, new_iso_path)
+            logging.info(f"Done! Renamed '{file_name}' to '{new_file_name}'")
+            shutil.rmtree(temp_dir, ignore_errors=True)
+        except Exception as e:
+            utils.log_warning_with_error(f"Error renaming '{file_name}' to '{new_file_name}', skipping", e)
+            continue
 
-    shutil.rmtree(temp_dir, ignore_errors=True)
-    logging.info("Renaming complete.")
-
-def reorder_files(input_dir):
+def reorder_files(items):
     """
     Orders files alphabetically.
     """
     try:
-        pass
-        # TODO
+        new_items = copy.deepcopy(items)
+        new_items.sort(key=lambda item: utils.sanitize_filename_ordering(item))
+        list_files(new_items)
     except Exception as e:
-        logging.error(f"An error occurred while reordering files: {e}")
+        utils.log_warning_with_error(f"Error ordering files alphabetically", e)
 
 def main():
     """
     Main function to display a menu and handle user input.
     """
-    parser = argparse.ArgumentParser(description="Manage your PSP library.")
+    parser = argparse.ArgumentParser(description="Manage your PSP library")
     parser.add_argument(
         "-i",
         "--input",
         required=True,
-        help="Directory containg a known PSP structure.",
+        help="Directory containg a known PSP structure",
     )
     
     args = parser.parse_args()
     input_dir = args.input
     
     if not os.path.isdir(input_dir):
-        logging.error(f"Directory '{input_dir}' does not exist.")
+        logging.error(f"Directory '{input_dir}' does not exist")
         return
+      
+    directory = utils.PSP(input_dir)
 
     while True:
-        print("\n1. List")
-        print("2. Rename")
-        print("3. Order (A-Z)")
-        print("4. Reload drive")
-        print("5. Exit")
-        choice = input("Enter your choice: ")
-        print()
+        choice = utils.render_menu(
+            [f"Current directory: {input_dir}", f"Total games/tools: {len(directory.items)}"],
+            ["List", "Rename", "Order", "Reload drive", "Exit"]
+        )
         
         if choice == "1":
-            list_files(input_dir)
+            list_files(directory.items)
         elif choice == "2":
-            rename_files(input_dir)
+            rename_files(directory.items)
         elif choice == "3":
-            reorder_files(input_dir)
+            reorder_files(directory.items)
         elif choice == "4":
+            pass
+        elif choice == "5":
             print("Exiting...")
             break
         else:
-            print("Invalid choice. Please try again.")
+            print("Invalid choice. Please try again")
         
 if __name__ == "__main__":
-    main()
+  try:
+      main()
+  except Exception as e:
+      logging.error(e)
+      exit()
--- src/utils.py
+++ src/utils.py
@@ -1,158 +1,233 @@
+import gc
 import os
 import re
-import platform
-import subprocess
+import sys
 import struct
-import binascii
 import logging
-from datetime import datetime
-from enum import Enum
+import pycdlib
+import binascii
+import platform
 
-class SearchTypes(Enum):
+from enum import Enum
+from datetime import datetime
+
+class SearchQueries(Enum):
+    """
+    Describes relevant files for searching purposes
+    KEY = EXTENSION (except for directory)
+    """
     ISO = ".iso"
     DIR = "dir"
-
-def extract_sfo(iso_path, temp_dir):
+    
+class Folders(Enum):
     """
-    Extracts the PARAM.SFO file from a given ISO file using 7-Zip.
+    Describes known directories that have relevant files
+    KEY = PATH_TO_DIR, [SEARCH_QUERY_1, SEARCH_QUERY_2, ...]
     """
-    try:
-        extract_command = ["7z", "x", iso_path, "PSP_GAME/PARAM.SFO", f"-o{temp_dir}", "-y"]
-        subprocess.run(extract_command, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        logging.info(f"Successfully extracted PARAM.SFO from {iso_path}")
-        return os.path.join(temp_dir, "PSP_GAME", "PARAM.SFO")
-    except subprocess.CalledProcessError as e:
-        logging.error(f"Error extracting PARAM.SFO from {iso_path}: {e}")
-        return None
+    ISO = "ISO", [SearchQueries.ISO]
+    GAME = "PSP\\GAME", [SearchQueries.DIR, SearchQueries.ISO]
+    
+class PSP:
+    def __init__(self, path):
+        self.path = path
+        self.directories = self.read_directories()
+        self.items = self.load_directories()
 
-def parse_sfo(sfo_path, param):
+    def read_directories(self):
+        """
+        Returns every potentially relevant directory based on the input
+        """
+        directories = []
+        for folder in Folders:
+            directories.append({
+                "name": folder.name,
+                "path": os.path.join(self.path, folder.value[0]),
+                "queries": folder.value[1]
+            })
+        return directories
+      
+    def load_directories(self):
+        """
+        Goes through all directories and returns every relevant file/directory
+        """
+        items = []
+        for directory in self.directories:
+            for query in directory["queries"]:
+                items.extend(filter_directory(parse_directory(directory["path"]), query))
+        items.sort(key=lambda item: item["timestamp"], reverse=True)
+        return items
+
+def render_menu(headers, content):
     """
-    Parses the PARAM.SFO file and retrieves the value of the specified parameter.
-    Credits: Marshall Ward - https://github.com/marshallward/devita
+    Renders headers and content of an interactive menu using print().
     """
-    try:
-        with open(sfo_path, "rb") as sfo_file:
-            header_raw = sfo_file.read(20)
-            header = struct.unpack("<4s4BIII", header_raw)
-            param_names, param_values = ["SFO File Signature", "SFO File Version"], []
+    for idx in range(len(headers)):
+        print(("\n" if idx == 0 else "") + headers[idx])
+        
+    for idx in range(len(content)):
+        print((f"\n{idx + 1}. " if idx == 0 else f"{idx + 1}. ") + content[idx])
+        
+    choice = input("\nEnter your choice: ")
+    print()
+    return choice
 
-            param_values.append(header[0].decode("utf-8"))
-            param_values.append(".".join([str(h) for h in header[1:5]]))
-
-            name_table_start = header[5]
-            data_table_start = header[6]
-            n_params = header[7]
-
-            def_table_bytes = 16 * n_params
-            name_table_bytes = data_table_start - name_table_start
-            def_table_padding = name_table_start - 20 - def_table_bytes
-
-            assert def_table_padding >= 0
-
-            def_table = []
-            for _ in range(n_params):
-                def_rec_raw = sfo_file.read(16)
-                def_record = struct.unpack("<HHIII", def_rec_raw)
-                def_table.append(def_record)
-
-            sfo_file.read(def_table_padding)
-
-            for e in range(n_params):
-                try:
-                    p_name_bytes = def_table[e + 1][0] - def_table[e][0]
-                except IndexError:
-                    p_name_bytes = name_table_bytes - def_table[e][0]
-                p_name = sfo_file.read(p_name_bytes).decode("utf-8")
-                param_names.append(p_name.rstrip("\x00"))
-
-            for e in range(n_params):
-                v_type = def_table[e][1]
-                v_total = def_table[e][3]
-                value_raw = sfo_file.read(v_total)
-
-                if v_type in (0x0204, 0x0004):
-                    value = value_raw.decode("utf-8").rstrip("\x00")
-                elif v_type == 0x0404:
-                    value_ascii = binascii.hexlify(value_raw[::-1]).decode("utf-8")
-                    value = int(value_ascii, 16)
-                else:
-                    value = None
-
-                param_values.append(value)
-
-            sfo_data = dict(zip(param_names, param_values))
-            logging.info(f"Successfully parsed PARAM.SFO: {param} = {sfo_data.get(param)}")
-            return sfo_data.get(param)
-    except Exception as e:
-        logging.error(f"Error parsing PARAM.SFO: {e}")
-        return None
-
-def sanitize_filename(filename):
+def log_warning_with_error(str, e):
     """
-    Sanitizes a filename by removing invalid characters.
+    Logs both an error and a warning when an exception is caught.
     """
-    try:
-        if not filename: return None
-        sanitized = re.sub(r'[<>:"/\\|?*]', "", filename)
-        logging.debug(f"Sanitized filename: {filename} -> {sanitized}")
-        return sanitized.strip().rstrip(".")
-    except:
-        logging.error(f"Error sanitizing file name: {filename}")
-        return None
-  
-def creation_date(path_to_file, query):
+    logging.warning(e)
+    logging.warning(str)
+
+def get_creation_date(path_to_file, query):
     """
     Try to get the date that a file/directory was created/last modified at, based on query.
     Credits: Mark Amery - https://stackoverflow.com/a/39501288
     """
-    try:
-        if query == SearchTypes.ISO:
-            if platform.system() == "Windows":
-                return os.path.getctime(path_to_file)
-            else:
-                stat = os.stat(path_to_file)
-                return stat.st_birthtime
-        elif query == SearchTypes.DIR:
-            return os.path.getmtime(path_to_file)
-        else: raise Exception()
-    except:
-        logging.error(f"Error getting creation date of: {path_to_file}")
-        return None
+    if query == SearchQueries.ISO:
+        if platform.system() == "Windows":
+            return os.path.getctime(path_to_file)
+        else:
+            stat = os.stat(path_to_file)
+            return stat.st_birthtime
+    elif query == SearchQueries.DIR:
+        return os.path.getmtime(path_to_file)
+    else: raise Exception()
 
 def parse_directory(dir):
     """
     Retrieves files or directories from a directory.
     """
-    return [] if not os.path.isdir(dir) else [{ "name": item, "path": os.path.join(dir, item)} for item in os.listdir(dir)]
-  
+    return [] if not os.path.isdir(dir) else [{ "name": item, "path": os.path.join(dir, item), "directory": dir} for item in os.listdir(dir)]
+
 def filter_directory(dir, query):
     """
     Filters an array of files or directories based on query.
     Returns their names, paths, and creation/last modified dates.
     """
     def process_file(file, is_valid):
-        name, path = file["name"], file["path"]
+        name, path, dir = file["name"], file["path"], file["directory"]
         if is_valid(path):
             try:
-                file_creation_date = creation_date(path, query)
-                to_readable_date = datetime.fromtimestamp(file_creation_date).strftime("%Y-%m-%d %H:%M:%S")
+                file_creation_date = get_creation_date(path, query)
+                to_readable_date = datetime.fromtimestamp(file_creation_date).strftime("%d-%m-%Y %H:%M:%S")
             except:
-                raise Exception(f"Could not retrieve {"creation" if query == SearchTypes.ISO else "last modified"} date for: {path}")
+                raise Exception(f"Could not retrieve {"creation" if query == SearchQueries.ISO else "last modified"} date for: {path}")
               
             return {
+                "category": query,
                 "name": name,
                 "path": path,
                 "timestamp": file_creation_date,
                 "date": to_readable_date,
+                "directory": dir
             }
         return None
 
-    if query == SearchTypes.ISO:
-        is_valid = lambda path: os.path.isfile(path) and path.lower().endswith(SearchTypes.ISO.value)
-    elif query == SearchTypes.DIR:
+    if query == SearchQueries.ISO:
+        is_valid = lambda path: os.path.isfile(path) and path.lower().endswith(SearchQueries.ISO.value)
+    elif query == SearchQueries.DIR:
         is_valid = os.path.isdir
     else:
         return []
 
     result = [process_file(file, is_valid) for file in dir]
     return [item for item in result if item is not None]
+  
+def join_with_script_path(file):
+    """
+    Returns a path for a temporary folder in the directory of the script.
+    """
+    return os.path.join(os.path.dirname(os.path.realpath(sys.argv[0])), file)
+
+def extract_sfo(iso_path):
+    """
+    Extracts the PARAM.SFO file from a given ISO file using pycdlib.
+    """
+    iso = pycdlib.PyCdlib()
+    temp_dir = join_with_script_path("temp")
+    
+    with open(iso_path, "rb") as b:
+        iso.open_fp(b)
+        os.makedirs(temp_dir, exist_ok=True)
+        extracted_file_path = os.path.join(temp_dir, "PARAM.SFO")
+        
+        with open(extracted_file_path, "wb") as f:
+            iso.get_file_from_iso_fp(f, iso_path="/PSP_GAME/PARAM.SFO")
+        iso.close()
+    return extracted_file_path, temp_dir
+
+def parse_sfo(sfo_path, param):
+    """
+    Parses the PARAM.SFO file and retrieves the value of the specified parameter.
+    Credits: Marshall Ward - https://github.com/marshallward/devita
+    """
+    with open(sfo_path, "rb") as sfo_file:
+        header_raw = sfo_file.read(20)
+        header = struct.unpack("<4s4BIII", header_raw)
+        param_names, param_values = ["SFO File Signature", "SFO File Version"], []
+
+        param_values.append(header[0].decode("utf-8"))
+        param_values.append(".".join([str(h) for h in header[1:5]]))
+
+        name_table_start = header[5]
+        data_table_start = header[6]
+        n_params = header[7]
+
+        def_table_bytes = 16 * n_params
+        name_table_bytes = data_table_start - name_table_start
+        def_table_padding = name_table_start - 20 - def_table_bytes
+
+        assert def_table_padding >= 0
+
+        def_table = []
+        for _ in range(n_params):
+            def_rec_raw = sfo_file.read(16)
+            def_record = struct.unpack("<HHIII", def_rec_raw)
+            def_table.append(def_record)
+
+        sfo_file.read(def_table_padding)
+
+        for e in range(n_params):
+            try:
+                p_name_bytes = def_table[e + 1][0] - def_table[e][0]
+            except IndexError:
+                p_name_bytes = name_table_bytes - def_table[e][0]
+            p_name = sfo_file.read(p_name_bytes).decode("utf-8")
+            param_names.append(p_name.rstrip("\x00"))
+
+        for e in range(n_params):
+            v_type = def_table[e][1]
+            v_total = def_table[e][3]
+            value_raw = sfo_file.read(v_total)
+
+            if v_type in (0x0204, 0x0004):
+                value = value_raw.decode("utf-8").rstrip("\x00")
+            elif v_type == 0x0404:
+                value_ascii = binascii.hexlify(value_raw[::-1]).decode("utf-8")
+                value = int(value_ascii, 16)
+            else:
+                value = None
+
+            param_values.append(value)
+
+        sfo_data = dict(zip(param_names, param_values))
+        return sfo_data.get(param)
+
+def sanitize_filename(filename):
+    """
+    Sanitizes a filename by removing invalid characters.
+    """
+    if not filename: return None
+    sanitized = re.sub(r'[<>:"/\\|?*]', "", filename)
+    sanitized = " ".join(sanitized.split())
+    return sanitized.strip().rstrip(".")
+  
+def sanitize_filename_ordering(file):
+    """
+    Sanitizes a filename for better alphabetical ordering.
+    """
+    filename = file["name"]
+    filename = filename.lower()
+    filename = filename[:-len(file["category"].value)] if file["category"] != SearchQueries.DIR else filename
+    filename = re.sub(r"[^a-zA-Z0-9]", "", filename)
+    return filename
